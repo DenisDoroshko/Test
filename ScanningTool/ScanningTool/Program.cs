01359f0f0ae8e87de74d3befd4d97a5a13a02e19
Dzianis Daroshka <dzianis_daroshka@epam.com>
2022-04-06 15:49:20 +0300

    test

---
+++ ScanningTool/ScanningTool/Program.cs
@@ -0,0 +1,26 @@
+﻿if (args.Length < 1)
+{
+    Console.WriteLine("There are no args, exit.");
+    return;
+}
+
+switch (args[0])
+{
+    case "help":
+        if (args.Length == 1)
+        {
+            Console.WriteLine("some information.");
+            break;
+        }
+
+        Console.WriteLine("Help does not allow parameters.");
+        break;
+    default:
+        Console.WriteLine("Given args:");
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+        break;
+}
