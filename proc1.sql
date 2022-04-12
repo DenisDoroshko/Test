63b156cdae2486b63e4e1e5298445a3484bde381
Dzianis Daroshka <dzianis_daroshka@epam.com>
2022-04-12 10:09:24 +0300

    Update proc1.sql

--- proc1.sql
+++ proc1.sql
@@ -1,5 +1,6 @@
   CREATE OR ALTER PROCEDURE [dbo].[MyProc1]
 	@id int
+	@room bigint
   AS 
 	DECLARE @int var1;
 	IF ( @c = 0 ) 
@@ -9,11 +10,16 @@
 	RETURN 0
   
   CREATE OR ALTER PROCEDURE [dbo].[MyProc2]
-	@id bigint OUTPUT
+	@id double OUTPUT
   AS 
 	DECLARE @int var1;
 	IF ( @c = 0 ) 
 	BEGIN
 		RETURN -60003
 	END
+	RETURN 0
+	
+  CREATE OR ALTER PROCEDURE [dbo].[MyProc2]
+	@id double OUTPUT
+  AS 
 	RETURN 0
