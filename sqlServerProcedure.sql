2f85823cee13231a53816908c855f3ab9cface97
Dzianis Daroshka <dzianis_daroshka@epam.com>
2022-04-12 13:50:14 +0300

    Update sqlServerProcedure.sql

--- sqlServerProcedure.sql
+++ sqlServerProcedure.sql
@@ -1,5 +1,6 @@
   CREATE OR ALTER PROCEDURE [dbo].[MyProc1]
-	@id int
+	@id int,
+	@name NVARCHAR(20)
   AS 
 	DECLARE @int var1;
 	IF ( @c = 0 ) 
@@ -9,11 +10,18 @@
 	RETURN 0
   
   CREATE OR ALTER PROCEDURE [dbo].[MyProc2]
-	@id bigint OUTPUT
+        @name NVARCHAR(20),
+	@id bigint OUTPUT,
+	@flat int OUTPUT,
   AS 
 	DECLARE @int var1;
 	IF ( @c = 0 ) 
 	BEGIN
 		RETURN -60003
 	END
-	RETURN 0
+	RETURN 10
+	
+CREATE OR ALTER PROCEDURE [dbo].[MyProc2]
+	@id bigint,
+  AS 
+	RETURN 10
