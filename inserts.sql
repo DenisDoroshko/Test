155557d181fc7e39c893803cbe6a42a973555b9c
Dzianis Daroshka <dzianis_daroshka@epam.com>
2022-04-18 15:59:30 +0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,4 +1,4 @@
-INSERT State(stateId,name,abbreviation,listOrder) VALUES(1,'Alabama',SELECT name from names where id = 123,'1')
+INSERT State(stateId,name,abbreviation,listOrder) VALUES(1,'Alabama',(SELECT name from names where id = 123),'1')
 INSERT SeqAccountActivityId (x) VALUES (NULL)
 INSERT INTO mytable (id, enumcode, comment) VALUES (123, 14, '')
 
